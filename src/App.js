361592867dba20a7d21c36578663406115a36564
NaufalRZ-aa <seanutwall@gmail.com>
2020-11-17 17:30:01 +0700

    Belajar Membuat Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,4 +17,43 @@ function App() {
   );
 }
 
-export default App;
+const AppVar = ()=>{
+  const nama = "Naufal Rizky";
+  const absen = 23;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Nama: {nama}</p>
+        <p>No. Absen: {absen}</p>
+      </header>
+    </div>
+  )
+}
+
+class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.nama = "Naufal Rizky";
+    this.absen = 23;
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Nama: {this.nama}</p>
+          <p>No. Absen: {this.absen}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export {
+  App,
+  AppVar,
+  AppClass
+};
